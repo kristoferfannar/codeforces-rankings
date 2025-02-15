88e8e089498c34f8d9536a3e15c2136a3adce185
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2025-02-15 01:44:56 -0500

    recalculate users's scores every hour

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,19 @@ import Submissions from "./submissions";
 
 const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
+const formatDate = (currentDate: Date): string => {
+	return new Intl.DateTimeFormat("en-US", {
+		weekday: "long",
+		year: "numeric",
+		month: "long",
+		day: "2-digit",
+		hour: "2-digit",
+		minute: "2-digit",
+		second: undefined,
+		hour12: true,
+	}).format(currentDate);
+};
+
 const fetchOldRating = async (user: User) => {
 	let oldRating = user.rating;
 	try {
@@ -73,134 +86,141 @@ const fetchSubmissions = async (
 	}
 };
 
-let users: User[] = [];
-export default async function Page() {
-	const names = [
-		"ArieArya",
-		"Bill_IRL",
-		"C_big_hand",
-		"Dani22reich",
-		"FeudalCheese",
-		"JunyiLou2",
-		"KenD2002",
-		"Kryogenesis",
-		"LiuH",
-		"LixiaCW2",
-		"Michael_Mint",
-		"MillionDev",
-		"PicNick",
-		"RobinHCL",
-		"Splaver",
-		"Splaver2",
-		"Ulanov_Victor",
-		"ZakT",
-		"aelee706",
-		"arth17singh",
-		"caseschemmer",
-		"chrispower43",
-		"codingcat420222",
-		"colleenqgl",
-		"ctg2140",
-		"flappyBird123",
-		"greekFreak224",
-		"guhasada",
-		"humza_choudry",
-		"ikaurov",
-		"jeewon",
-		"jeffreyhuang1009",
-		"jessicard",
-		"joonrhee",
-		"josh2775",
-		"jz514",
-		"kalcy",
-		"kechenliu",
-		"khaliung",
-		"kingsley.u",
-		"kirby5098",
-		"kristoferfannar",
-		"laraj723",
-		"ljs2225",
-		"lsig",
-		"mastranj",
-		"mektronus",
-		"michael-mih",
-		"mjstraus2304",
-		"nataliecclaire",
-		"ns3886",
-		"ogbangjo",
-		"philluple",
-		"porkbarrel",
-		"ppusarla",
-		"rahulram04",
-		"ruby931109",
-		"samdobson28",
-		"schlange03",
-		"shriyamahakala",
-		"sr3992",
-		"ss7172",
-		"sy3245",
-		"t_0ra",
-		"tomg1448",
-		"tora20",
-		"twy123",
-		"vxl3n",
-		"yitongta",
-		"zacharyzusin",
-	];
+const getInfo = async (handles: string[]): Promise<User[]> => {
+	const response = await fetch(
+		`https://codeforces.com/api/user.info?handles=${handles.join(";")}`,
+	);
 
-	const getInfo = async (handles: string[]): Promise<User[]> => {
-		const response = await fetch(
-			`https://codeforces.com/api/user.info?handles=${handles.join(";")}`,
-		);
-
-		const data: { status: string; result: UserDTO[] } = await response.json();
-		if (!response.ok || data.status !== "OK") {
-			throw new Error("failed");
-		}
-
-		const fetched = data.result.map((out): User => {
-			return {
-				handle: out.handle,
-				rating: out.rating ?? 0,
-				maxRating: out.maxRating ?? 0,
-			};
-		});
-
-		fetched.sort((a, b) => {
-			return a.rating >= b.rating ? -1 : 1;
-		});
-
-		const users: User[] = [];
-
-		for (const user of fetched) {
-			console.log(`processing "${user.handle}"`);
-			await delay(20);
-			const oldRating = await fetchOldRating(user);
-			await delay(20);
-			const { totalSubmissions, recentSubmissions } =
-				await fetchSubmissions(user);
-
-			users.push({
-				...user,
-				ratingChange: user.rating - oldRating,
-				totalSubmissions: totalSubmissions,
-				recentSubmissions: recentSubmissions,
-			});
-		}
-
-		return users;
-	};
-
-	if (users.length === 0) {
-		users = await getInfo(names);
+	const data: { status: string; result: UserDTO[] } = await response.json();
+	if (!response.ok || data.status !== "OK") {
+		throw new Error("failed");
 	}
 
+	const fetched = data.result.map((out): User => {
+		return {
+			handle: out.handle,
+			rating: out.rating ?? 0,
+			maxRating: out.maxRating ?? 0,
+		};
+	});
+
+	fetched.sort((a, b) => {
+		return a.rating >= b.rating ? -1 : 1;
+	});
+
+	const users: User[] = [];
+
+	for (const user of fetched) {
+		console.log(`processing "${user.handle}"`);
+		await delay(20);
+		const oldRating = await fetchOldRating(user);
+		await delay(20);
+		const { totalSubmissions, recentSubmissions } =
+			await fetchSubmissions(user);
+
+		users.push({
+			...user,
+			ratingChange: user.rating - oldRating,
+			totalSubmissions: totalSubmissions,
+			recentSubmissions: recentSubmissions,
+		});
+	}
+
+	return users;
+};
+
+const names = [
+	"ArieArya",
+	"Bill_IRL",
+	"C_big_hand",
+	"Dani22reich",
+	"FeudalCheese",
+	"JunyiLou2",
+	"KenD2002",
+	"Kryogenesis",
+	"LiuH",
+	"LixiaCW2",
+	"Michael_Mint",
+	"MillionDev",
+	"PicNick",
+	"RobinHCL",
+	"Splaver",
+	"Splaver2",
+	"Ulanov_Victor",
+	"ZakT",
+	"aelee706",
+	"arth17singh",
+	"caseschemmer",
+	"chrispower43",
+	"codingcat420222",
+	"colleenqgl",
+	"ctg2140",
+	"flappyBird123",
+	"greekFreak224",
+	"guhasada",
+	"humza_choudry",
+	"ikaurov",
+	"jeewon",
+	"jeffreyhuang1009",
+	"jessicard",
+	"joonrhee",
+	"josh2775",
+	"jz514",
+	"kalcy",
+	"kechenliu",
+	"khaliung",
+	"kingsley.u",
+	"kirby5098",
+	"kristoferfannar",
+	"laraj723",
+	"ljs2225",
+	"lsig",
+	"mastranj",
+	"mektronus",
+	"michael-mih",
+	"mjstraus2304",
+	"nataliecclaire",
+	"ns3886",
+	"ogbangjo",
+	"philluple",
+	"porkbarrel",
+	"ppusarla",
+	"rahulram04",
+	"ruby931109",
+	"samdobson28",
+	"schlange03",
+	"shriyamahakala",
+	"sr3992",
+	"ss7172",
+	"sy3245",
+	"t_0ra",
+	"tomg1448",
+	"tora20",
+	"twy123",
+	"vxl3n",
+	"yitongta",
+	"zacharyzusin",
+];
+let users: User[] = [];
+users = await getInfo(names);
+let lastUpdated: Date = new Date();
+
+setInterval(
+	async () => {
+		users = await getInfo(names);
+		lastUpdated = new Date();
+	},
+	60 * 60 * 1000,
+);
+
+export default async function Page() {
 	return (
 		<div className="m-8 border-black ">
 			<div className="flex justify-center gap-32">
 				<Rankings users={users} />
 				<Submissions users={users} />
 			</div>
+			<p>{`last updated at ${formatDate(lastUpdated)}`}</p>
 		</div>
 	);
 }
