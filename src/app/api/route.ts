9e10e6b02296038bebac499d61097a7ce89bb82c
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2025-02-17 13:38:54 -0500

    setup edge-config storage

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,8 +1,8 @@
-import { getLastUpdated, getUsers } from "../data";
+import { get } from "@vercel/edge-config";
 
 export async function GET(request: Request) {
-    const users = getUsers();
-    const lastUpdated = getLastUpdated();
+	const users = await get("users");
+	const lastUpdated = await get("lastUpdated");
 
-    return Response.json({ users, lastUpdated });
+	return Response.json({ users, lastUpdated });
 }
